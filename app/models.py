e79f7fba15e4eab3fb484f065ca61e86cdd78a7d
haffi112 <haffi.e@gmail.com>
2024-11-03 18:24:15 +0000

    Yay

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     role = db.Column(db.String(20), default='regular')
     preferred_language = db.Column(db.String(10))
     interface_language = db.Column(db.String(10))
